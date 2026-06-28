49c9d43b6b1b5be0a95c0abf798724922ba59a47
thesargampraveen <sargampraveen3@gmail.com>
2026-06-28 14:19:23 +0530

    commit;

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import heroImg from './assets/hero.png'
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<section id="center">
+<section id="center"> 
   <div class="hero">
     <img src="${heroImg}" class="base" width="170" height="179">
     <img src="${typescriptLogo}" class="framework" alt="TypeScript logo"/>
