a0e44f1ae17a602f9dd8e549824e0a02cd792ba7
thesargampraveen <sargampraveen3@gmail.com>
2026-07-13 20:28:13 +0530

    forgetpass:

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Signup from "./Pages/SignUp/SignUp";
 import Login from "./Pages/SignIn/Login";
+import ForgetPass from "./Pages/ForgotPassword/ForgetPass";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/forgot-password" element={<ForgetPass />} />
       </Routes>
     </BrowserRouter>
   );
