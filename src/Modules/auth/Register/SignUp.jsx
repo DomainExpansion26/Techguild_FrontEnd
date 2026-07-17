de402ab127324ba51b1dcac07853550c27dd1b9c
thesargampraveen <sargampraveen3@gmail.com>
2026-07-17 10:09:27 +0530

    resolve2:

--- src/Modules/auth/Register/SignUp.jsx
+++ src/Modules/auth/Register/SignUp.jsx
@@ -1,9 +1,5 @@
 import { useState } from "react";
-<<<<<<< HEAD:src/Modules/auth/Register/SignUp.jsx
 import { Link, useNavigate } from "react-router-dom";
-=======
-import { Link } from "react-router-dom";
->>>>>>> fa3ef46 (login:):src/Pages/SignUp/SignUp.jsx
 import { Mail, Lock, User } from "lucide-react";
 import "./SignUp.css";
 import {
