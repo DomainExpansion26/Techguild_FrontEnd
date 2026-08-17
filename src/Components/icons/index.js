fa8fc8962d0f253ce3ffcfc44dc5bd359b3bdc5b
Ridhi Purwar <ridhipurwar2002@gmail.com>
2026-08-17 08:21:13 +0530

    feat: create a verfication screen and update folder structure

--- src/Components/icons/index.js
+++ src/Components/icons/index.js
@@ -14,19 +14,31 @@ import CircleQuestionMark from "../../assets/icons/circle-question-mark.svg?reac
 import Mail from "../../assets/icons/mail.svg?react";
 import Check from "../../assets/icons/check.svg?react";
 import ArrowLeft from "../../assets/icons/arrow-left.svg?react";
-import Lock from "../../assets/icons/lock.svg?react";
-import Eye from "../../assets/icons/eye.svg?react";
-import EyeOff from "../../assets/icons/eye-off.svg?react";
-import Google from "../../assets/icons/google.svg?react";
-import GitHub from "../../assets/icons/github.svg?react";
-import Building2 from "../../assets/icons/building2.svg?react";
+import ArrowRight from "../../assets/icons/arrow-right.svg?react";
+import Search from "../../assets/icons/search.svg?react";
+import UserCheck from "../../assets/icons/user-check.svg?react";
+import CreditCard from "../../assets/icons/credit-card.svg?react";
+import IdCard from "../../assets/icons/id-card.svg?react";
+import IdCardLanyard from "../../assets/icons/id-card-lanyard.svg?react";
+import Clock9 from "../../assets/icons/clock9.svg?react";
+import Lightbulb from "../../assets/icons/lightbulb.svg?react";
 import Briefcase from "../../assets/icons/briefcase.svg?react";
+import CheckCircle2 from "../../assets/icons/check-circle2.svg?react";
+import Menu from "../../assets/icons/menu.svg?react";
+import X from "../../assets/icons/x.svg?react";
+import ChevronRight from "../../assets/icons/chevron-right.svg?react";
+import ChevronDown from "../../assets/icons/chevron-down.svg?react";
 
 export {
   ArrowLeft,
+  ArrowRight,
+  Search,
   LayoutDashboard,
   User2,
-  User,
+  UserCheck,
+  CreditCard,
+  IdCard,
+  IdCardLanyard,
   Files,
   FileText,
   Group,
@@ -39,11 +51,12 @@ export {
   CircleQuestionMark,
   Mail,
   Check,
-  Lock,
-  Eye,
-  EyeOff,
-  Google,
-  GitHub,
-  Building2,
+  Clock9,
+  Lightbulb,
   Briefcase,
+  CheckCircle2,
+  Menu,
+  X,
+  ChevronRight,
+  ChevronDown,
 };
