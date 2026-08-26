21c668b61224ffe3844e1af41fd9dc755e49fcb2
Ridhi Purwar <ridhipurwar2002@gmail.com>
2026-08-26 19:25:46 +0530

    fix:resolve conflict

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import AccountType from "./Modules/auth/Accountype/Accounttype";
 import DashBoard from "./Modules/Individual/Screen/Pages/DashBoard/DashBoard";
 import ClientDashboard from "./Modules/Client/Pages/Dashboard/Dashboard";
 import ClientProfile from "./Modules/Client/Pages/Profile/Profile";
-import WholeProfile from "./Modules/Client/Pages/whole_Profile";
+import ClientWholeProfile from "./Modules/Client/Pages/whole_Profile";
 import ClientQuestBoard from "./Modules/Client/Pages/QuestBoard/QuestBoard";
 import ClientApplications from "./Modules/Client/Pages/Applications/Applications";
 import ClientActiveQuests from "./Modules/Client/Pages/ActiveQuests/ActiveQuests";
@@ -59,9 +59,8 @@ function App() {
       <Route path="/client-dashboard" element={<ClientDashboard />} />
       <Route path="/client-profile" element={<ClientProfile />} />
       <Route path="/client-profile/:step" element={<ClientProfile />} />
-      <Route path="/client-whole-profile" element={<WholeProfile />} />
-      <Route path="/whole-profile" element={<WholeProfile />} />
-      <Route path="/client-profile/whole" element={<WholeProfile />} />
+      <Route path="/client-whole-profile" element={<ClientWholeProfile />} />
+      <Route path="/client-profile/whole" element={<ClientWholeProfile />} />
       <Route path="/client-quest-board" element={<ClientQuestBoard />} />
       <Route path="/client-quest-board/:questId" element={<ClientQuestBoard />} />
       <Route path="/client-applications" element={<ClientApplications />} />
